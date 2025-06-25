3cd500228e99c8ae4ec4b52e385d7087f9f55751
Arndt, Tyler <Tyler.Arndt+asb@associatedbank.com>
2025-06-25 16:22:27 -0500

    fix: updating

--- variables.tf
+++ variables.tf
@@ -3,12 +3,12 @@ type = map(string)
 
 validation {
     condition = length(var.baseline_tags.app) >= 3
-    error_message = "The value for baseline_tags.app is not 3 chracters in length or greater."
+    error_message = "The value for baseline_tags.app is not 3 chracters in length or greater"
 }
 
 validation {
     condition = var.baseline_tags.cost_center >= 100 && var.baseline_tags.cost_center <= 999999
-    error_message = "The value for baseline_tags.cost_center must be between 100 and 999999."
+    error_message = "The value for baseline_tags.cost_center must be between 100 and 999999"
 }
 
 validation {
@@ -18,7 +18,7 @@ validation {
 
 validation {
     condition = length(var.baseline_tags.team) >= 3 && length(var.baseline_tags.team) <= 6 
-    error_message = "The value for baseline_tags.team must be atleast 3 characters long but not longer than 6."
+    error_message = "The value for baseline_tags.team must be atleast 3 characters long but not longer than 6"
 }
 
 }
